754e3ab44ce5795442b4349e3f485ca2f0d4eaa2
fabienDROMMER <fabien.drommer@gmail.com>
2020-11-29 14:01:05 +0100

    ajout gestion echap + annuler dans dialogs

--- interface.sh
+++ interface.sh
@@ -21,10 +21,17 @@ function consult {
 		done
 		IFS=$' '
 		SORTIE=$(dialog --clear --title "Liste des messages" \
-		--menu "Choix du message" 15 100 10 "${options[@]}" 2>&1 > /dev/tty)
-		infos_fichier=$(gpg -q --decrypt --armor "${files[$SORTIE]}")
-		dialog --clear --msgbox "Destinataires : $(echo "$infos_fichier" | jq ".destinataires") \nObjet : $(echo "$infos_fichier" | jq ".objet") \nMessage : $(echo "$infos_fichier" | jq ".message")" 0 0 
+				--menu "Choix du message" 15 100 10 "${options[@]}" 2>&1 > /dev/tty)
+		if [ $? -gt 0 ];then
+		    break;
+		else
+		    infos_fichier=$(gpg -q --decrypt --armor "${files[$SORTIE]}")
+		dialog --clear --msgbox "Destinataires : $(echo "$infos_fichier" | jq ".destinataires") \nObjet : $(echo "$infos_fichier" | jq ".objet") \nMessage : $(echo "$infos_fichier" | jq ".message")" 0 0
+		fi
+		
+		
 	fi
+	
 }
 
 function interfaceDepart {
@@ -39,20 +46,27 @@ function interfaceDepart {
 	INPUT=/tmp/menu.sh.$$
 	ARRET=false
 	while [ $ARRET = "false" ]; do
-		dialog --clear --title "Dialog Test" \
-		--menu "Choix de l'action" 20 100 3 \
-		Send "Send a message" \
-		Consult "Consult your messages" \
-		Exit "Quit the program" 2>"${INPUT}"
-		menuitem=$(<"${INPUT}")
-		case $menuitem in
-		 Send) interfaceEnvoiMessage;;
-		 Consult) consult;;
-		 Exit) 
-			ARRET=true
-			break;;
-		esac
+	    dialog --clear --title "Dialog Test" \
+		   --menu "Choix de l'action" 20 100 3 \
+		   Send "Send a message" \
+		   Consult "Consult your messages" \
+		   Exit "Quit the program" 2>"${INPUT}"
+	    menuitem=$(<"${INPUT}")
+	    exit=$?
+	    if [ $exit -eq 0 ];then
+		ARRET=true;
+	    fi
+	    case $menuitem in
+		Send) ARRET=false
+		      interfaceEnvoiMessage;;
+		Consult) ARRET=false
+			 consult;;
+		Exit) ARRET=true
+		      break;;
+	    esac
+	    
 	done
+	
 }
 
 function interfaceEnvoiMessage {
@@ -63,6 +77,7 @@ function interfaceEnvoiMessage {
 	testNull=$(verifMessage res)
 	testUser=$(verifUsers $utilisateurs)
 	echo "$testUser testUser"
+	ARRET=false
 	while [ $testNull -ge 1 -o $testUser -ge 1 ]
 	do
 		VALUES=$(dialog --ok-label "Envoyer" \
@@ -73,6 +88,9 @@ function interfaceEnvoiMessage {
 			"Destinataires:"         1 1	"" 	1 15 85 500 \
 			"        Objet:"         3 1	""  	3 15 85 500 \
 			"     Messsage:"         5 1	""  	5 15 85 1000 2>&1 > /dev/tty)
+		if [ $? -gt 0  ]; then
+		    break;
+		fi
 		compteur=0
 		IFS=$'\n'
 		y=($VALUES)
@@ -83,6 +101,7 @@ function interfaceEnvoiMessage {
 		IFS=$' '
 		testNull=$(verifMessage $utilisateurs $objet $message)
 		testUser=$(verifUsers $utilisateurs)
+		
 	done
 	envoyerMessage "$utilisateurs" "$objet" "$message"
 }
